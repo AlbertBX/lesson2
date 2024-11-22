72d994963ab6c1b371e29729ec6138dd7242f52f
AlbertBX <avh1972@mail.ru>
2024-11-22 22:31:32 +0500

    Update lesson_2_task_3.py

--- lesson_2_task_3.py
+++ lesson_2_task_3.py
@@ -1,9 +1,9 @@
 import math
 
-def square(a):
-    return math.ceil(a**2)
-
 num_a = float(input("Введите длину стороны квадрата: "))
-print(f"Площадь квадрата: {square(num_a)}")
 if num_a<=0: print("Пожалуйста, вводите только положительные числа!")
+else:
+    def square(a):
+      if num_a>=0: return math.ceil(a**2)
+    print(f"Площадь квадрата: {square(num_a)}")
 	
