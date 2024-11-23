1bc90e76f74b19e7dfe1207b42879375991e8aa5
AlbertBX <avh1972@mail.ru>
2024-11-23 21:45:46 +0500

    Update lesson_2_task_2.py

--- lesson_2_task_2.py
+++ lesson_2_task_2.py
@@ -1,12 +1,14 @@
 def is_year_leap(year):
     if year % 4==0:
-        print ("True")
+        return True
     else:
-        print ("False")
+        return False
+        
 try:
     year = int(input("Введите целое число года: "))
-    is_year_leap(year)
+    result=is_year_leap(year)
+    print(f"год {year}: {result}")
 except ValueError:
-    print("Пожалуйста, введите целое число.")
+    print("Пожалуйста, введите целое число!")
 
    
